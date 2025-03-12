7e5603c2762769628622d2f7641c1dfe8f45eb2c
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-12 23:45:53 +0530

    send mail

---
+++ server/utils/sendMail.js
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import Mustache from "mustache";
+import fs from "fs"; 
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+
+export const sendMail = async (
+  name = "Guest",
+  receiverMail = "",
+  subject = "No subject",
+  resetLink = ""
+) => {
+  let transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.SENDER_EMAIL,
+      pass: process.env.SENDER_EMAIL_APP_PASSWORD,
+    },
+  });
+
+  // Get the __dirname equivalent in ES modules
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  // Construct the template file path
+    const templatePath = path.join(__dirname, "../templates", "reset-password.html");
+    console.log({templatePath});
+    
+  
+  try {
+    // Read the template file
+    const template = fs.readFileSync(templatePath, "utf8");
+
+    // Render the email content using Mustache
+    const renderedEmail = Mustache.render(template, { name, resetLink });
+
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: receiverMail,
+      subject: subject,
+      html: renderedEmail,
+    };
+
+    // Send the email
+    const info = await transport.sendMail(mailOptions);
+    return info;
+  } catch (err) {
+    console.error("Error sending email:", err);
+    return err;
+  }
+};
