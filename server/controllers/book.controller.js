2562ce7586a20df43cbdcd278428120d06d5499f
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-13 19:28:44 +0530

    download excel api

--- server/controllers/book.controller.js
+++ server/controllers/book.controller.js
@@ -1,4 +1,5 @@
 import Book from "../models/book.model.js";
+import ExcelJS from "exceljs";
 
 export const addBook = async (req, res) => {
   const { name, author } = req.body;
@@ -59,19 +60,59 @@ export const borrowBook = async (req, res) => {
       },
     });
 
-
     if (!book) {
       return res.status(404).json({ message: "No book found" });
     }
     if (book.borrowed) {
-      return res
-        .status(400)
-        .json({
-          message: `Book is already borrowed by ${book.borrower?.profile?.first_name} ${book.borrower?.profile?.last_name}`,
-        });
+      return res.status(400).json({
+        message: `Book is already borrowed by ${book.borrower?.profile?.first_name} ${book.borrower?.profile?.last_name}`,
+      });
     }
     await Book.findByIdAndUpdate(bookId, { borrowed: true, borrower: userId });
     return res.status(200).json({ message: "Book borrowed" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const downloadExcel = async (req, res) => {
+  try {
+    const bookCount = await Book.countDocuments();
+
+    if (bookCount === 0) {
+      return res
+        .status(404)
+        .json({ message: "No books available to download." });
+    }
+
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader("Content-Disposition", "attachment; filename=books.xlsx");
+
+    const workbook = new ExcelJS.stream.xlsx.WorkbookWriter({
+      stream: res,
+      useStyles: true,
+    });
+    const worksheet = workbook.addWorksheet("Books");
+    worksheet.columns = [
+      { header: "Book Name", key: "name", width: 50 },
+      { header: "Author", key: "author", width: 50 },
+    ];
+    worksheet.getRow(1).font = { bold: true, size: 18 };
+
+    Book.find({}, "name author")
+      .cursor() // Create a cursor to iterate over the results without loading everything into memory
+      .eachAsync((book,index) => {
+        worksheet.addRow({
+          name: book.name,
+          author: book.author,
+        });
+      })
+      .then(() => {
+        workbook.commit();
+      });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- server/routes/book.routes.js
+++ server/routes/book.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { addBook, fetchBooks, borrowBook } from "../controllers/book.controller.js";
+import { addBook, fetchBooks, borrowBook, downloadExcel } from "../controllers/book.controller.js";
 
 const router = express.Router();
 
 router.post("/", addBook)
 router.get("/", fetchBooks)
 router.post("/borrow/:bookId", borrowBook)
+router.get("/download-excel", downloadExcel)
 
 export default router;
