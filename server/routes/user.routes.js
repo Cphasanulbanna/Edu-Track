8e206e1831e5907de57181fddd32f5a3dd58f376
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-13 11:22:23 +0530

    student listing api

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 
-import { fetchUsers } from "../controllers/user.controller.js";
+import { fetchUsers, updateProfile, fetchStudents } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
+import upload from "../middleware/upload.middleware.js";
 
 const router = express.Router();
 
-router.get("/", authMiddleware(["admin"]), fetchUsers);
+router.get("/", authMiddleware(["admin","student"]), fetchUsers);
+router.post("/update-profile", upload.single("image"), updateProfile)
+router.get("/students", fetchStudents)
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,41 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import session from "express-session";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import { connectDb } from "./config/db.js";
+import passport from "./middleware/googleOauth.middleware.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false, // In development mode, set this to false
+      httpOnly: true
+    },
+  })
+);
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:3000"],
+    credentials: true
+  })
+);
 app.use(express.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
