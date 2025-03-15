75d12882c29de51de12d3d8ab25f00a8dda39b1b
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-15 15:12:12 +0530

    db helper function

--- server/helper/db/db.helper.js
+++ server/helper/db/db.helper.js
@@ -1,13 +1,15 @@
+import mongoose from "mongoose";
+
 export async function checkIndexes(Model) {
   const indexes = await Model.collection.getIndexes();
   console.log(indexes);
 }
 
 export async function dropAnIndex(indexName, ModelName) {
-  const User = mongoose.model(ModelName);
+  const model = mongoose.model(ModelName);
 
   try {
-    await User.collection.dropIndex(indexName);
+    await model.collection.dropIndex(indexName);
     console.log(`${indexName} index dropped successfully` );
   } catch (error) {
     console.error("Error dropping index:", error);
