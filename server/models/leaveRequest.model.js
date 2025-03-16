4d2cf2a88d1c423badd1db2cd5d2ffc7b2df86a5
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-16 15:11:06 +0530

    leave request schema

---
+++ server/models/leaveRequest.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const leaveRequestSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    approvedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    fromDate: { type: Date, required: true },
+    toDate: { type: Date, required: true },
+    reason: { type: String, required: true },
+    status: { type: String, enum: ["pending", "approved", "denied"] },
+  },
+  { timestamps: true }
+);
+
+const LeaveRequest = mongoose.model("LeaveRequest", leaveRequestSchema);
+
+export default LeaveRequest;
