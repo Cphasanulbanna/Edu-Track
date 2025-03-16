f047b69b5dbc7f3f43cebd316d993dfa001b470b
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-16 13:14:12 +0530

    date formatting

--- server/constant/constant.js
+++ server/constant/constant.js
@@ -1,5 +1,1 @@
-import dayjs from "dayjs";
-
 export const LIBRARY_JOB_TIME = '0 8 * * 1'
-
-export const COLLEGE_CHECK_IN_TIME =  dayjs().hour(9).minute(0).second(0).millisecond(0);
--- server/utils/date.js
+++ server/utils/date.js
@@ -1,5 +1,12 @@
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js";
 
-export const getTimeFromDate = (date) => {
-    return dayjs(date).format('HH:mm:ss');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export const  convertUTCtoIST = (date) => {
+    return dayjs(date).tz("Asia/Kolkata").format("YYYY-MM-DD HH:mm:ss");
 }
+
+export const COLLEGE_CHECK_IN_TIME = dayjs().hour(9).minute(0).second(0).millisecond(0);
