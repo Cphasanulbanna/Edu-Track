4dab326ce324eea0c2fbd97de12cb7c327c3e420
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-18 22:36:11 +0530

    payment model

---
+++ server/models/payment.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const paymentSchema = new mongoose.Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    semester: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Semester",
+    },
+    amount: { type: Number, required: true },
+    feeType: {
+      type: String,
+      enum: [
+        "Semester Fee",
+        "Exam Fee",
+        "Event Fee",
+        "Library Fee",
+        "Hostel Fee",
+      ],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["paid", "pending", "failed"],
+      default: "pending",
+    },
+    paymentDate: { type: Date, default: Date.now },
+    transactionId: { type: String, unique: true },
+  },
+  { timestamps: true }
+);
+
+const Payment = mongoose.model("Payment", paymentSchema);
+
+export default Payment;
