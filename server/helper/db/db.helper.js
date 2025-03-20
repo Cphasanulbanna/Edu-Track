ac482998219743fca04250d5fcd9cf879e3b4ada
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-20 19:10:22 +0530

    changes

--- server/helper/db/db.helper.js
+++ server/helper/db/db.helper.js
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import mongoose from "mongoose";
 
 export async function checkIndexes(Model) {
@@ -10,9 +11,34 @@ export async function dropAnIndex(indexName, ModelName) {
 
   try {
     await model.collection.dropIndex(indexName);
-    console.log(`${indexName} index dropped successfully` );
+    console.log(`${indexName} index dropped successfully`);
   } catch (error) {
     console.error("Error dropping index:", error);
   }
 }
 
+export const updatePaymentDates = async (ModelName) => {
+  try {
+    const model = mongoose.model(ModelName);
+    const payments = await model.find({}, { _id: 1, paymentDate: 1 });
+
+    const bulkOperations = payments.map((payment) => ({
+      updateOne: {
+        filter: { _id: payment._id },
+        update: {
+          $set: {
+            paymentDate: dayjs(payment.paymentDate).format("YYYY-MM-DD"),
+          },
+        },
+      },
+    }));
+
+    if (bulkOperations.length > 0) {
+      await model.bulkWrite(bulkOperations);
+    } else {
+      console.log("No records found to update.");
+    }
+  } catch (error) {
+    console.error("Error updating payment dates:", error);
+  }
+};
