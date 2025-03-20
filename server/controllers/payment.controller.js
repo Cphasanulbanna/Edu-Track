a496a68654f975d3f7827ddb44867c8a12068a2c
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-20 19:10:01 +0530

    transactions apis

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -3,7 +3,10 @@ import { razorpay } from "../config/razorpay.js";
 import crypto from "node:crypto";
 import { validateWebhookSignature } from "razorpay/dist/utils/razorpay-utils.js";
 import Payment from "../models/payment.model.js";
+import Semester from "../models/semester.model.js";
+import User from "../models/user.model.js";
 import mongoose from "mongoose";
+import dayjs from "dayjs";
 
 export const createRazorpayOrder = async (req, res) => {
   const { amount } = req.body;
@@ -22,6 +25,12 @@ export const createRazorpayOrder = async (req, res) => {
       if (!semesterId) {
         return res.status(400).json({ message: "Semester Id is required" });
       }
+      const semester = await Semester.findById(semesterId);
+      if (amount !== semester.feeAmount) {
+        return res
+          .status(400)
+          .json({ message: `Semester fee should be ${semester.feeAmount}` });
+      }
     }
 
     const paymentRecord = new Payment({
@@ -32,6 +41,7 @@ export const createRazorpayOrder = async (req, res) => {
       amount,
       feeType,
       status: "pending",
+      paymentDate: dayjs(Date.now()).format("YYYY-MM-DD"),
     });
 
     await paymentRecord.save();
@@ -43,7 +53,7 @@ export const createRazorpayOrder = async (req, res) => {
     };
 
     const options = {
-      amount,
+      amount: Number(amount) * 100,
       currency: "INR",
       receipt: `${feeType}-${uuid?.slice(0, 20)}`,
       notes,
@@ -90,33 +100,46 @@ export const verifyPayment = async (req, res) => {
 };
 
 const handlePaymentSuccess = async (payload) => {
-  const paymentRecord = new Payment({
-    student: mongoose.Types.ObjectId.createFromHexString(
-      payload?.notes?.studentId
-    ),
-    semester: payload?.notes?.semesterId
-      ? mongoose.Types.ObjectId.createFromHexString(payload?.notes?.semesterId)
-      : null,
-    amount: payload?.notes?.amount,
-    feeType: payload?.notes?.feeType,
-    status: "paid",
-  });
-  await paymentRecord.save();
+  console.log({ successPayload: payload });
+
+  await Payment.findOneAndUpdate(
+    { transactionId: payload?.id },
+    {
+      student: mongoose.Types.ObjectId.createFromHexString(
+        payload?.notes?.studentId
+      ),
+      semester: payload?.notes?.semesterId
+        ? mongoose.Types.ObjectId.createFromHexString(
+            payload?.notes?.semesterId
+          )
+        : null,
+      amount: parseFloat((payload?.amount / 100).toFixed(2)),
+      feeType: payload?.notes?.feeType,
+      status: "paid",
+      method: payload?.method,
+    }
+  );
 };
 
 const handlePaymentFailure = async (payload) => {
-  const paymentRecord = new Payment({
-    student: mongoose.Types.ObjectId.createFromHexString(
-      payload?.notes?.studentId
-    ),
-    semester: payload?.notes?.semesterId
-      ? mongoose.Types.ObjectId.createFromHexString(payload?.notes?.semesterId)
-      : null,
-    amount: payload?.notes?.amount,
-    feeType: payload?.notes?.feeType,
-    status: "failed",
-  });
-  await paymentRecord.save();
+  console.log({ failurePayload: payload });
+  await Payment.findOneAndUpdate(
+    { transactionId: payload?.order_id },
+    {
+      student: mongoose.Types.ObjectId.createFromHexString(
+        payload?.notes?.studentId
+      ),
+      semester: payload?.notes?.semesterId
+        ? mongoose.Types.ObjectId.createFromHexString(
+            payload?.notes?.semesterId
+          )
+        : null,
+      amount: parseFloat((payload?.amount / 100).toFixed(2)),
+      feeType: payload?.notes?.feeType,
+      status: "failed",
+      method: payload?.method,
+    }
+  );
 };
 
 export const handlePaymentStatus = async (req, res) => {
@@ -129,6 +152,8 @@ export const handlePaymentStatus = async (req, res) => {
 
   try {
     if (isValid) {
+      console.log({ isValid });
+
       const { event, payload } = req.body;
 
       switch (event) {
@@ -145,6 +170,172 @@ export const handlePaymentStatus = async (req, res) => {
     } else {
       return res.status(400).json({ message: "Invalid webhook signature" });
     }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const fetchAllTransactions = async (req, res) => {
+  const { status = "", date, page = 1, limit = 2 } = req.query;
+  const elementsToSkip = (page - 1) * limit;
+  const matchStage = {};
+  if (status) {
+    matchStage.status = status;
+  }
+  if (date) {
+    const formattedDate = dayjs(date, "DD-MM-YYYY")
+      .format("YYYY-MM-DD")
+      .toString();
+    matchStage.paymentDate = formattedDate;
+  }
+  try {
+    const aggregationPipeline = [
+      {
+        $facet: {
+          transactions: [
+            {
+              $match: { ...matchStage },
+            },
+            {
+              $lookup: {
+                from: "users",
+                localField: "student",
+                foreignField: "_id",
+                as: "user",
+              },
+            },
+            { $unwind: "$user" },
+            {
+              $lookup: {
+                from: "profiles",
+                localField: "user.profile",
+                foreignField: "_id",
+                as: "profile",
+              },
+            },
+            { $unwind: "$profile" },
+            {
+              $lookup: {
+                from: "semesters",
+                localField: "semester",
+                foreignField: "_id",
+                as: "semester",
+              },
+            },
+            { $unwind: "$semester" },
+            {
+              $sort: {
+                paymentDate: -1,
+              },
+            },
+            {
+              $project: {
+                amount: 1,
+                status: 1,
+                paymentDate: 1,
+                feeType: 1,
+                semester: {
+                  semesterNumber: "$semester.semesterNumber",
+                  feeAmount: "$semester.feeAmount",
+                },
+                student: {
+                  firstName: "$profile.first_name",
+                  lastName: "$profile.last_name",
+                },
+                transactionId: 1,
+              },
+            },
+            {
+              $skip: elementsToSkip,
+            },
+            {
+              $limit: limit,
+            },
+          ],
+          totalPaidAmount: [
+            {
+              $match: { status: "paid" },
+            },
+            {
+              $group: {
+                _id: null,
+                total: { $sum: "$amount" },
+              },
+            },
+          ],
+          totalTransactions: [
+            {
+              $count: "count",
+            },
+          ],
+          totalSuccessfulTransactions: [
+            {
+              $match: { status: "paid" },
+            },
+            {
+              $count: "count",
+            },
+          ],
+          totalFailedTransactions: [
+            {
+              $match: { status: "failed" },
+            },
+            {
+              $count: "count",
+            },
+          ],
+        },
+      },
+    ];
+    const [result] = await Payment.aggregate(aggregationPipeline);
+    const {
+      transactions,
+      totalPaidAmount,
+      totalSuccessfulTransactions,
+      totalFailedTransactions,
+      totalTransactions,
+    } = result;
+
+    if (!transactions?.length) {
+      return res.status(404).json({ message: "No transactions found" });
+    }
+    const totalPages = Math.ceil(
+      totalSuccessfulTransactions?.[0]?.count / limit
+    );
+    return res.status(200).json({
+      transactions: transactions,
+      totalPaidAmount: totalPaidAmount[0].total,
+      totalSuccessfulTransactions: totalSuccessfulTransactions?.[0]?.count,
+      totalFailedTransactions: totalFailedTransactions?.[0]?.count,
+      totalTransactions: totalTransactions?.[0]?.count,
+      page,
+      limit,
+      totalPages,
+      itemsInPage: totalSuccessfulTransactions?.[0]?.count,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const fetchTransactionsOfStudent = async (req, res) => {
+  const { studentId } = req.params;
+  try {
+    if (!studentId) {
+      return res.status(400).json({ message: "Student Id is required" });
+    }
+    const student = await User.findById(studentId);
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    const transactions = await Payment.find({ student: studentId }).sort({
+      paymentDate: -1,
+    });
+    if (!transactions.length) {
+      return res.status(404).json({ message: "No transactions found" });
+    }
+    return res.status(200).json({ transactions });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- server/routes/payment.route.js
+++ server/routes/payment.route.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { createRazorpayOrder, handlePaymentStatus, verifyPayment } from "../controllers/payment.controller.js";
+import { createRazorpayOrder, fetchAllTransactions, fetchTransactionsOfStudent, handlePaymentStatus, verifyPayment } from "../controllers/payment.controller.js";
+import { verifyCaptcha } from "../middleware/recaptcha.middleware.js";
 
 
 const router = express.Router();
 
-router.post("/create-order", createRazorpayOrder);
+router.post("/create-order", verifyCaptcha, createRazorpayOrder);
 router.post("/verify-payment", verifyPayment);
 router.post("/webhook", handlePaymentStatus);
+router.get("/history", fetchAllTransactions);
+router.get("/history/:studentId", fetchTransactionsOfStudent);
 
 export default router;
