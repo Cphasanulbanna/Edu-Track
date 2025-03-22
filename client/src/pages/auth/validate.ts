c6e4e63f99c0e8642a778d84de045539cc2ed4d8
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-22 22:07:42 +0530

    validations

--- client/src/pages/auth/validate.ts
+++ client/src/pages/auth/validate.ts
@@ -2,22 +2,27 @@ import { SignUp } from "@/types/forms";
 import { z, ZodType } from "zod";
 
 export const signUpSchema: ZodType<SignUp> = z.object({
-  first_name: z.string().min(3, { message: "First name is required" }),
+  first_name: z
+    .string({ required_error: "First name is required" })
+    .min(3, { message: "Last name must be at least 3 characters" }),
 
   last_name: z
-    .string()
+    .string({ required_error: "Last name is required" })
     .min(2, { message: "Last name must be at least 2 characters" }),
 
-  email: z.string().email({ message: "Please enter a valid email address" }),
-  password: z
+  email: z
     .string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Please enter a valid email address" }),
+  password: z
+    .string({ message: "Password is required" })
     .min(6, { message: "Password must be at least 6 characters" })
     .regex(/[A-Z]/, { message: "Must include an uppercase letter" })
     .regex(/[a-z]/, { message: "Must include a lowercase letter" })
     .regex(/\d/, { message: "Must include a number" }),
 
   mobile_number: z
-    .string()
+    .string({ message: "Mobile number is required" })
     .min(10, { message: "Mobile number must be at least 10 digits" })
     .max(10, { message: "Mobile number must be no more than 10 digits" })
     .regex(/^\d+$/, { message: "Mobile number must be numeric" }),
