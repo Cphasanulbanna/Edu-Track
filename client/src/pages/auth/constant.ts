16cf39807be3bff24fe55223e60cc2b2770b8ffe
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-22 21:26:24 +0530

    default value assignment

--- client/src/pages/auth/constant.ts
+++ client/src/pages/auth/constant.ts
@@ -1,9 +1,9 @@
 export const SLICE_KEY = "AUTH";
 
 export const signUpDefaultValues = {
-  email: null,
-  firstName: null,
-  lastName: null,
-  password: null,
-  mobileNumber: null,
+  email: "",
+  firstName: "",
+  lastName: "",
+  password: "",
+  mobileNumber: "",
 };
