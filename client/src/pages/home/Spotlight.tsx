144b29686b0cd885df7c0b9cc908664ac5f968ae
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-23 17:56:56 +0530

    spotlight ui

---
+++ client/src/pages/home/Spotlight.tsx
@@ -0,0 +1,12 @@
+const Spotlight = () => {
+  return (
+      <div className="bg-[url('/images/college-bg.jpg')] bg-cover h-screen">
+          <div className="flex flex-col gap-3 justify-center items-center h-full">
+              
+          <h1 className="text-3xl font-bold text-primary-foreground">Welcome</h1>
+          </div>
+    </div>
+  )
+}
+
+export default Spotlight
