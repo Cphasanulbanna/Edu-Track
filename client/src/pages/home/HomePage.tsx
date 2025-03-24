c2c642ce1609046117652adc0b5af909be6b5628
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-24 14:01:38 +0530

    home

---
+++ client/src/pages/home/HomePage.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/layout/Header'
+import Spotlight from './Spotlight'
+
+const HomePage = () => {
+  return (
+      <div>
+          <Header />
+          <Spotlight />
+    </div>
+  )
+}
+
+export default HomePage
