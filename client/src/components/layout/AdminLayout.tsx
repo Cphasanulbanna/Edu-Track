4e97bd29dd1a3d677f528895207d06977d857d44
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-24 14:01:57 +0530

    layouts

---
+++ client/src/components/layout/AdminLayout.tsx
@@ -0,0 +1,46 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import SidebarComponent from "./SidebarComponent";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { actions as commonSliceActions } from "@/common/slice";
+
+  const items = [
+    {
+      title: "Students",
+      url: "students",
+      icon: "PiStudentFill",
+    },
+    {
+      title: "Teachers",
+      url: "teachers",
+      icon: "GiTeacher",
+    },
+    {
+      title: "Staff",
+      url: "#",
+      icon: "FaUsers",
+    },
+  ];
+const AdminLayout = () => {
+  const dispatch = useDispatch();
+
+
+
+  useEffect(() => {
+    dispatch(commonSliceActions.setSidebarData({ name: "Admin", items }));
+  }, [ dispatch]);
+  return (
+    <div>
+      <div className="fixed top-0 left-0 z-40 right-0">
+        <Header />
+      </div>
+      <div className="w-screen h-screen flex justify-between gap-5">
+        <SidebarComponent />
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
