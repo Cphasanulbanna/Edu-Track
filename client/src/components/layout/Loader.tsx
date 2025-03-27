dffd1974bc386b64d21ea81d1830c957392ea27e
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-27 17:44:59 +0530

    loader ui

---
+++ client/src/components/layout/Loader.tsx
@@ -0,0 +1,11 @@
+import Spinner from "../custom/Spinner";
+
+const Loader = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Spinner />
+    </div>
+  );
+};
+
+export default Loader;
