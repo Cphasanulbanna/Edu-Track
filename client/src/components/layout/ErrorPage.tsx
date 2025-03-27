a356ae925d22992a1dfa368f5af44f445baf4a67
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-27 17:45:08 +0530

    error page ui

---
+++ client/src/components/layout/ErrorPage.tsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-secondary">
+      Something went wrong
+    </div>
+  );
+};
+
+export default ErrorPage;
