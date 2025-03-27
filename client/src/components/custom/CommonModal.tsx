88c29fc2f0256b7472ba28b23c18115693626bf8
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-27 17:44:51 +0530

    common modal ui

---
+++ client/src/components/custom/CommonModal.tsx
@@ -0,0 +1,34 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogOverlay,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+
+interface CommonModalProps {
+  title: string;
+  description?: string;
+  children?: React.ReactElement
+  open: boolean;
+  close?: ()=> void
+}
+const CommonModal = ({ title, description,children,open= false,close }: CommonModalProps) => {
+  return (
+    <Dialog onOpenChange={close} open={open} >
+      <DialogOverlay  />
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CommonModal;
