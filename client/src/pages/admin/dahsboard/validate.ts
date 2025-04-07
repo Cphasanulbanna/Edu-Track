0f3b1db08d96da9cc9eb7a0f1aedd2788809f944
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-04-07 15:47:09 +0530

    batch form validation

--- client/src/pages/admin/dahsboard/validate.ts
+++ client/src/pages/admin/dahsboard/validate.ts
@@ -6,3 +6,9 @@ export const createCourseSchema = z.object({
     .min(2, { message: "Course name should be minimum 2 characters" }),
 });
 export type CreateCourseType = z.infer<typeof createCourseSchema>;
+
+export const createBatchSchema = z.object({
+  department: z.string({ required_error: "Batch is required" }),
+  year: z.string({ required_error: "Batch year is required" }),
+});
+export type CreateBatchType = z.infer<typeof createBatchSchema>;
