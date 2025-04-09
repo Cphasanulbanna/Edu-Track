d8767af50797eaf771641d11c12d5d1677638276
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-04-09 16:23:10 +0530

    route updates

--- client/src/pages/auth/routes.tsx
+++ client/src/pages/auth/routes.tsx
@@ -2,7 +2,10 @@ import AuthLayout from "@/components/layout/AuthLayout";
 import Header from "@/components/layout/Header";
 import LogInPage from "@/pages/auth/components/LogInPage";
 import Signup from "@/pages/auth/components/Signup";
+import RedirectPage from "@/RedirectPage";
 import { Route, Routes } from "react-router-dom";
+import ForgetPasswordPage from "./components/ForgetPasswordPage";
+import ResetPasswordPage from "./components/ResetPassword";
 
 const AuthRoutes = () => {
   return (
@@ -10,6 +13,9 @@ const AuthRoutes = () => {
       <Header />
       <Routes>
         <Route path="/" element={<AuthLayout />}>
+          <Route path="/auth/google/callback" element={<RedirectPage />} />
+          <Route path="/forget-password" element={<ForgetPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<LogInPage />} />
         </Route>
--- client/src/routes/AppRoute.tsx
+++ client/src/routes/AppRoute.tsx
@@ -2,19 +2,15 @@
 import { Route, Routes } from "react-router-dom";
 import { lazy } from "react";
 
-const AdminRoutes = lazy(() => import("./AdminRoutes"))
-const AuthRoutes = lazy(() => import("./AuthRoutes"))
+const AdminRoutes = lazy(() => import("@/pages/admin/routes"))
+const AuthRoutes = lazy(() => import("@/pages/auth/routes"))
 const AppRoute = () => {
   return (
 
       <Routes>
-        {/* <Route path="/auth/google/callback" element={<RedirectPage />} />
-        <Route path="/forget-password" element={<ForgetPasswordPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        {/*
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/static-map" element={<StaticGoogleMap />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<LogInPage />} />
         <Route path="/" element={<HomePage />} /> */}
           <Route path="/admin/*" element={<AdminRoutes />} />
           <Route path="/auth/*" element={<AuthRoutes />} />
