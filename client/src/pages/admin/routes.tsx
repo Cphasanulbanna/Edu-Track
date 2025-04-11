a44c148aef6bcaf7acec9e9c33592f7dfd8bc277
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-04-11 15:01:44 +0530

    route fix

--- client/src/pages/admin/routes.tsx
+++ client/src/pages/admin/routes.tsx
@@ -5,12 +5,13 @@ import StudentsList from "@/pages/admin/dahsboard/components/StudentsList";
 import TeachersList from "@/pages/admin/dahsboard/components/TeachersList";
 import Transactions from "@/pages/admin/dahsboard/components/Transactions";
 import CourseDetails from "./dahsboard/components/CourseDetails";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const AdminRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<AdminLayout  />}>
+      <Route path="/" element={<AdminLayout />}>
+        <Route index element={<Navigate to="students" replace />} />
         <Route path="/students" element={<StudentsList />} />
         <Route path="/teachers" element={<TeachersList />} />
         <Route path="/transactions" element={<Transactions />} />
