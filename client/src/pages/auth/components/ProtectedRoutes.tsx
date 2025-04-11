c924cb52db151fef2d5a19073269530c11b704a1
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-04-11 14:40:48 +0530

    protected route validation

---
+++ client/src/pages/auth/components/ProtectedRoutes.tsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import { getIsAuthenticated } from "../selector";
+import { Navigate } from "react-router-dom";
+import { PropsWithChildren } from "react";
+import UnAuthorized from "./UnAuthorized";
+
+interface ProtectedRoutesProps extends PropsWithChildren {
+  roles?: string[];
+}
+
+const ProtectedRoutes = ({ children, roles = [] }: ProtectedRoutesProps) => {
+  const isAuth = useSelector(getIsAuthenticated);
+  const currentRoles = JSON.parse(localStorage.getItem("role") as string);
+  const isValidRoles = currentRoles?.some((role: string) =>
+    roles?.includes(role)
+  );
+  if (!isAuth) {
+    return <Navigate to={"/auth/login"} />;
+  }
+  if (!isValidRoles) {
+    return <UnAuthorized />;
+  }
+  return <>{children}</>;
+};
+
+export default ProtectedRoutes;
--- client/src/routes/AppRoute.tsx
+++ client/src/routes/AppRoute.tsx
@@ -2,8 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "@/pages/home/HomePage";
 import NotFound from "@/components/custom/NotFound";
 
-import AdminRoutes from"@/pages/admin/routes"
-import AuthRoutes from "@/pages/auth/routes"
+import AdminRoutes from "@/pages/admin/routes";
+import AuthRoutes from "@/pages/auth/routes";
+import ProtectedRoutes from "@/pages/auth/components/ProtectedRoutes";
+import { ROLES } from "@/common/constant";
 const AppRoute = () => {
   return (
     <Routes>
@@ -11,7 +13,14 @@ const AppRoute = () => {
         <Route path="/static-map" element={<StaticGoogleMap />} /> */}
       <Route path="*" element={<NotFound />} />
       <Route path="/" element={<HomePage />} />
-      <Route path="/admin/*" element={<AdminRoutes />} />
+      <Route
+        path="/admin/*"
+        element={
+          <ProtectedRoutes roles={[ROLES.ADMIN]}>
+            <AdminRoutes />
+          </ProtectedRoutes>
+        }
+      />
       <Route path="/auth/*" element={<AuthRoutes />} />
     </Routes>
   );
